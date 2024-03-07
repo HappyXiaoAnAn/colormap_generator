2165f66e78da1d4975e8c7dd2c0e08c867521a61
Happy小安安 <andy85728@gmail.com>
2024-03-07 20:29:27 +0800

    fix: use react to deal with popup feature
    popover attribute not work in firefox
    use React useRef() and useEffect instead
    refer to:
    https://medium.com/@shkim04/react-how-to-detect-
    click-outside-a-component-984fe2e003e8

--- src/PythonCode.jsx
+++ src/PythonCode.jsx
@@ -1,17 +1,57 @@
 import './button.css'
+import { useState, useRef, useEffect } from "react";
 
 export function PythonCode({coloroutrgb}) {
+    const [visible, setVisible] = useState(false);
     const python_code = output_function_for_matplotlib(coloroutrgb)
+
+    const toggleText = () => {
+        setVisible(prev => !prev);
+    };
+
     return (
         <>
             {/* <button popovertarget="my-popover" className='button-85'><span>Python</span></button> */}
-            <button popovertarget="my-popover" className='button-85'>
+            <button className='button-85' onClick={toggleText}>
                 <img src='https://matplotlib.org/stable/_static/logo_dark.svg' width={"100px"}/>
             </button>
-            <div popover="auto" id="my-popover">
-                <textarea value={python_code} style={{width: "500px", height: "200px"}} readOnly></textarea>
-            </div>
+            {
+                visible && (<TextArea python_code={python_code} onClick={toggleText}/>)
+            }
         </>
+    )
+}
+
+// * reference::
+//      https://medium.com/@shkim04/react-how-to-detect-click-outside-a-component-984fe2e003e8
+function TextArea(props) {
+    const wrapperRef = useRef();
+
+    useEffect(() => {
+      document.addEventListener('mousedown', handleClickListener);
+      
+      return () => {
+        document.removeEventListener('mousedown', handleClickListener);
+      };
+    }, []);
+  
+    const handleClickListener = (event) => {
+      let clickedInside;
+      if(props.exceptionRef) {
+        clickedInside = (wrapperRef && wrapperRef.current.contains(event.target)) || props.exceptionRef.current === event.target || props.exceptionRef.current.contains(event.target);
+      }
+      else {
+        clickedInside = (wrapperRef && wrapperRef.current.contains(event.target));
+      }
+  
+      if (clickedInside) return;
+      else props.onClick();
+    }
+
+    return (
+        <div ref={wrapperRef} className='popover'>
+            <textarea value={props.python_code} style={{width: "500px", height: "200px"}} readOnly></textarea>
+        </div>
     )
 }
 
