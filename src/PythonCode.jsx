0dfb63027546ed4f7b4a2c3b025137067c37c075
Happy小安安 <andy85728@gmail.com>
2024-04-13 17:04:38 +0800

    feat: decorate pythoncode textarea by @shoelace

--- src/PythonCode.jsx
+++ src/PythonCode.jsx
@@ -1,57 +1,23 @@
 import './button.css'
 import { useState, useRef, useEffect } from "react";
+import SlDialog from '@shoelace-style/shoelace/dist/react/dialog';
+import SlTextarea from '@shoelace-style/shoelace/dist/react/textarea';
 
 export function PythonCode({coloroutrgb}) {
     const [visible, setVisible] = useState(false);
     const python_code = output_function_for_matplotlib(coloroutrgb)
 
-    const toggleText = () => {
-        setVisible(prev => !prev);
-    };
-
     return (
         <>
             {/* <button popovertarget="my-popover" className='button-85'><span>Python</span></button> */}
-            <button className='button-85' onClick={toggleText}>
+            <button className='button-85' onClick={() => setVisible(true)}>
                 <img src='https://matplotlib.org/stable/_static/logo_dark.svg' width={"100px"}/>
             </button>
-            {
-                visible && (<TextArea python_code={python_code} onClick={toggleText}/>)
-            }
+            
+            <SlDialog label='Python code' open={visible}  onSlAfterHide={() => setVisible(false)} style={{'--width':'100%', textAlign: "left"}}>
+                <SlTextarea className='code-area' value={python_code} rows={15} size='small' readOnly></SlTextarea>
+            </SlDialog>
         </>
-    )
-}
-
-// * reference::
-//      https://medium.com/@shkim04/react-how-to-detect-click-outside-a-component-984fe2e003e8
-function TextArea(props) {
-    const wrapperRef = useRef();
-
-    useEffect(() => {
-      document.addEventListener('mousedown', handleClickListener);
-      
-      return () => {
-        document.removeEventListener('mousedown', handleClickListener);
-      };
-    }, []);
-  
-    const handleClickListener = (event) => {
-      let clickedInside;
-      if(props.exceptionRef) {
-        clickedInside = (wrapperRef && wrapperRef.current.contains(event.target)) || props.exceptionRef.current === event.target || props.exceptionRef.current.contains(event.target);
-      }
-      else {
-        clickedInside = (wrapperRef && wrapperRef.current.contains(event.target));
-      }
-  
-      if (clickedInside) return;
-      else props.onClick();
-    }
-
-    return (
-        <div ref={wrapperRef} className='popover'>
-            <textarea value={props.python_code} style={{width: "500px", height: "200px"}} readOnly></textarea>
-        </div>
     )
 }
 
